5dfd51d777356d52459ffd2fb37b667f7ab21a0c
josemiguelbento <jose.miguel.bento@gmail.com>
2022-11-04 11:33:47 +0000

    lab3 a funcionar

---
+++ lab3/lab3.m
@@ -0,0 +1,10 @@
+period_time = 1;
+sample_time = 0.03;
+miu = 0.025;
+coef_hibrido = [0 0.2 1 0.3 -0.4 -0.1 0.1 -0.05 -0.02 -0.01];
+
+%% 5_1 - Baralhador de dados
+
+sim('teste_bar_dados_5_1',Simulation_Time)
+
+
